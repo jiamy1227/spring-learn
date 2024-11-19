0f8b7e50ffb350df8d8f29e4f09739566b34fc6f
jiamy <1049465510@qq.com>
2024-11-19 13:51:28 +0800

    jms

---
+++ jms/src/main/java/com/jiamy/Main.java
@@ -0,0 +1,12 @@
+package com.jiamy;
+
+/**
+ * @description: ${description}
+ * @author: jiamy
+ * @create: 2024/10/11 14:36
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
